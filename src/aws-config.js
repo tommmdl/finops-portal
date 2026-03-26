12441e6636a915f36fc787f82dcbadb6b8531c35
Rafael Santiago <tomfks@hotmail.com>
2026-03-26 17:53:30 -0300

    debug: log amplify config on init

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,6 +1,6 @@
 import { Amplify } from 'aws-amplify'
 
-Amplify.configure({
+const config = {
   Auth: {
     Cognito: {
       userPoolId:       import.meta.env.VITE_COGNITO_USER_POOL_ID,
@@ -10,6 +10,15 @@ Amplify.configure({
       },
     },
   },
+}
+
+console.log('[FinOps] Amplify config:', {
+  userPoolId:       import.meta.env.VITE_COGNITO_USER_POOL_ID,
+  userPoolClientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
+  apiUrl:           import.meta.env.VITE_API_URL,
+  region:           import.meta.env.VITE_AWS_REGION,
 })
+
+Amplify.configure(config)
 
 export const API_URL = import.meta.env.VITE_API_URL || ''
